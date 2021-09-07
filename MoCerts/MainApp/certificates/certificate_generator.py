ed1897b87a886118cf9b6be6050bdeb55164a1c9
risercash <risercash@gmail.com>
2021-09-07 09:40:42 +0600

    Номер сертификата поправил

--- MoCerts/MainApp/certificates/certificate_generator.py
+++ MoCerts/MainApp/certificates/certificate_generator.py
@@ -8,7 +8,7 @@ def generate_certificate(nominal, number, user1, user2, user3):
     first_user = f'1. {user1.first_name} {user1.last_name}'
     second_user = f'2. {user2.first_name} {user2.last_name}'
     third_user = f'3. {user3.first_name} {user3.last_name}'
-    number_text = '№ ' + str(number)
+    number_text = str(number)
     init_image_name = f'cert_big_{nominal}.png'
     image_path = os.path.join(settings.MEDIA_DIR, init_image_name)
     font_path = os.path.join(settings.MEDIA_DIR, 'font.ttf')
@@ -19,8 +19,8 @@ def generate_certificate(nominal, number, user1, user2, user3):
     draw_text.text((24, 180), first_user, font=font)
     draw_text.text((41, 214), second_user, font=font)
     draw_text.text((62, 248), third_user, font=font)
-    font_number = ImageFont.truetype(font_path_number, 15)
-    draw_text.text((260, 370), number_text, font=font_number, fill='black', stroke_width=1)
+    font_number = ImageFont.truetype(font_path_number, size=18)
+    draw_text.text((260, 367), number_text, font=font_number, fill='black', stroke_width=1)
     file_name_result = f'{number}.png'
     file_name_result_path = os.path.join(settings.MEDIA_DIR, file_name_result)
     img.save(file_name_result_path)
