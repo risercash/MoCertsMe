97509c3d4fc2534ba4f3d3fd71a0d8b2467c72bd
risercash <risercash@gmail.com>
2021-09-09 18:29:14 +0600

    cert_generator

--- MoCerts/MainApp/certificates/certificate_generator.py
+++ MoCerts/MainApp/certificates/certificate_generator.py
@@ -4,7 +4,7 @@ import os
 
 
 def generate_certificate(nominal, number, user1, user2, user3):
-    ''' ===== Генератор нового сертификата ===== '''
+    """ ===== Генератор нового сертификата ===== """
     first_user = f'1. {user1.first_name} {user1.last_name}'
     second_user = f'2. {user2.first_name} {user2.last_name}'
     third_user = f'3. {user3.first_name} {user3.last_name}'
@@ -12,15 +12,15 @@ def generate_certificate(nominal, number, user1, user2, user3):
     init_image_name = f'cert_big_{nominal}.png'
     image_path = os.path.join(settings.MEDIA_DIR, init_image_name)
     font_path = os.path.join(settings.MEDIA_DIR, 'font.ttf')
-    font_path_number = os.path.join(settings.MEDIA_DIR, 'georgia.ttf')
+    font_path_number = os.path.join(settings.MEDIA_DIR, 'arial.ttf')
     img = Image.open(image_path)
     font = ImageFont.truetype(font_path, size=26)
     draw_text = ImageDraw.Draw(img)
     draw_text.text((24, 180), first_user, font=font)
     draw_text.text((41, 214), second_user, font=font)
     draw_text.text((62, 248), third_user, font=font)
-    font_number = ImageFont.truetype(font_path, size=18)
-    draw_text.text((260, 367), number_text, font=font_number, fill='black', stroke_width=1)
+    font_number = ImageFont.truetype(font_path_number, size=18)
+    draw_text.text((262, 369), number_text, font=font_number, fill='black', stroke_width=1)
     file_name_result = f'{number}.png'
     file_name_result_path = os.path.join(settings.MEDIA_DIR, file_name_result)
     img.save(file_name_result_path)
