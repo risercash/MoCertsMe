cc0d15f589f1c932bf0abd650c4480f29dfbdd00
risercash <risercash@gmail.com>
2021-09-10 09:44:21 +0600

    terms

--- MoCerts/MoCerts/prod_settings.py
+++ MoCerts/MoCerts/prod_settings.py
@@ -19,23 +19,23 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['mocerts.com', 'localhost', '127.0.0.1', '*']
 
-# Настройки для базы данных 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+#Настройки для базы данных
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MySQL_DB'),
-        'USER': os.environ.get('NAME_USER_DB'),
-        'PASSWORD': os.environ.get('PASSWORD_DB'),
-        'HOST': os.environ.get('HOST_DB'),
-        'PORT': os.environ.get('PORT_DB'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ.get('MySQL_DB'),
+#         'USER': os.environ.get('NAME_USER_DB'),
+#         'PASSWORD': os.environ.get('PASSWORD_DB'),
+#         'HOST': os.environ.get('HOST_DB'),
+#         'PORT': os.environ.get('PORT_DB'),
+#     }
+# }
 
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
