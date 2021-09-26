94f5b8ba3a2be6737ac8ee3d45d806035c7789af
Doszhan <doszhan.m@mail.ru>
2021-09-26 12:53:55 +0600

    manual crash

--- MoCerts/MainApp/models.py
+++ MoCerts/MainApp/models.py
@@ -112,7 +112,7 @@ class ManualPosts(models.Model):
     index_number = models.PositiveIntegerField(
         verbose_name='порядковый номер на странице',)
     title = models.CharField(max_length=255, blank=True, verbose_name='Заголовок',)
-    # description = models.CharField(max_length=255, blank=True, verbose_name='Описание')
+    description = models.CharField(max_length=255, blank=True, verbose_name='Описание')
     video = EmbedVideoField(blank=True, verbose_name='Ссылка на видео')
 
     class Meta:
