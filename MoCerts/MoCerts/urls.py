0fb51723986957b5287c973c95fb74e22225db4f
Doszhan <doszhan.m@mail.ru>
2021-09-27 05:36:22 +0600

    add robots.txt

--- MoCerts/MoCerts/urls.py
+++ MoCerts/MoCerts/urls.py
@@ -4,13 +4,15 @@ from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.http import HttpResponse
+
 
 urlpatterns = [
     path('nimda/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('MainApp.urls')),
-    # path('robots.txt/', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
+    path('robots.txt', lambda r: HttpResponse("User-agent: *\nDisallow: /admin", content_type="text/plain; charset=utf-8")),
     path('google2f64469d3bb2d616.html/', TemplateView.as_view(template_name="google2f64469d3bb2d616.html",)),
     # path('auth/', include('social_django.urls', namespace='social')),
 ]
