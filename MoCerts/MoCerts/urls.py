f4b5fef2a661d36a7db7e440f3a556f13c159dc5
Doszhan <doszhan.m@mail.ru>
2021-09-26 22:45:25 +0600

    add robots.txt

--- MoCerts/MoCerts/urls.py
+++ MoCerts/MoCerts/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.conf import settings
-from django.conf.urls.static import static # нужно прописать путь статика для отабражения изображении
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('nimda/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('MainApp.urls')),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
     # path('auth/', include('social_django.urls', namespace='social')),
 ]
 
