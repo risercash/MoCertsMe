3056f05adcef507f53436fdfae5c944c6377c23c
Doszhan <doszhan.m@mail.ru>
2021-09-27 17:44:01 +0600

    add favicon

--- MoCerts/MoCerts/urls.py
+++ MoCerts/MoCerts/urls.py
@@ -4,7 +4,8 @@ from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-from django.http import HttpResponse
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 
 urlpatterns = [
@@ -12,11 +13,9 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('MainApp.urls')),
-    path('google2f64469d3bb2d616.html/', TemplateView.as_view(template_name="google2f64469d3bb2d616.html",)),
-
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),),
 ]
 
 if settings.DEBUG:
-    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_DIRS)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
